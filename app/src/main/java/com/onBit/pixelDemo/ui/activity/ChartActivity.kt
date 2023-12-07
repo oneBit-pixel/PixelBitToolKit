65bcc90e29c4dae6513f0d64f84a03b4755596b0
wxm <123@>
2023-12-08 00:54:25 +0800

    增加绘制 圆角refener

--- app/src/main/java/com/onBit/pixelDemo/ui/activity/ChartActivity.kt
+++ app/src/main/java/com/onBit/pixelDemo/ui/activity/ChartActivity.kt
@@ -1,24 +1,15 @@
 package com.onBit.pixelDemo.ui.activity
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.view.LayoutInflater
-import com.github.mikephil.charting.components.Legend
-import com.github.mikephil.charting.components.MarkerView
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.data.BaseEntry
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
-import com.github.mikephil.charting.renderer.BubbleChartRenderer
-import com.github.mikephil.charting.utils.ColorTemplate
-import com.onBit.PixelBitToolKit.R
+import com.github.mikephil.charting.renderer.HorizontalBarChartRenderer
 import com.onBit.PixelBitToolKit.databinding.ActivityChartBinding
 import com.onBit.lib_base.base.BaseActivity
-import com.onBit.pixelDemo.ui.view.RoundedBarRenderer
+import com.onBit.pixelDemo.ui.view.RoundRenderer
 
 class ChartActivity : BaseActivity<ActivityChartBinding>() {
     override val bindingInflater: (LayoutInflater) -> ActivityChartBinding
@@ -27,16 +18,16 @@ class ChartActivity : BaseActivity<ActivityChartBinding>() {
 
     private val value by lazy {
         listOf(
-            BarEntry(0f, floatArrayOf(0f,75f, 120f)),
-            BarEntry(1f, floatArrayOf(0f,85f,100f)),
-            BarEntry(2f, floatArrayOf(0f,25f,100f)),
-            BarEntry(3f, floatArrayOf(0f,65f,100f)),
-            BarEntry(4f, floatArrayOf(0f,55f,100f)),
-            BarEntry(5f, floatArrayOf(0f,45f,100f)),
-            BarEntry(6f, floatArrayOf(0f,35f,100f)),
-            BarEntry(7f, floatArrayOf(0f,45f,100f)),
-            BarEntry(8f, floatArrayOf(0f,55f,100f)),
-            BarEntry(9f, floatArrayOf(0f,85f,100f)),
+            BarEntry(0f, floatArrayOf(75f, 120f)),
+            BarEntry(1f, floatArrayOf(85f,100f)),
+            BarEntry(2f, floatArrayOf(25f,100f)),
+            BarEntry(3f, floatArrayOf(65f,100f)),
+            BarEntry(4f, floatArrayOf(55f,100f)),
+            BarEntry(5f, floatArrayOf(45f,100f)),
+            BarEntry(6f, floatArrayOf(35f,100f)),
+            BarEntry(7f, floatArrayOf(45f,100f)),
+            BarEntry(8f, floatArrayOf(55f,100f)),
+            BarEntry(9f, floatArrayOf(85f,100f)),
         )
     }
 
@@ -44,7 +35,7 @@ class ChartActivity : BaseActivity<ActivityChartBinding>() {
         super.initView()
         mBinding.apply {
             val barDataSet = BarDataSet(value, "这是一个标题")
-            barDataSet.colors = listOf(Color.TRANSPARENT,Color.TRANSPARENT,Color.BLUE)
+            barDataSet.colors = listOf(Color.TRANSPARENT,Color.BLUE)
             barDataSet.valueTextColor=Color.RED
             val arrayListOf = arrayListOf<IBarDataSet>(barDataSet)
             val barData = BarData(arrayListOf)
@@ -53,9 +44,9 @@ class ChartActivity : BaseActivity<ActivityChartBinding>() {
 
             chart.setTouchEnabled(true)
             chart.data = barData
-            chart.setDrawValueAboveBar(false)
+            chart.renderer=RoundRenderer(chart,chart.animator,chart.viewPortHandler)
+            chart.setDrawValueAboveBar(true)
 
-            chart.renderer
         }
     }
 
---
+++ app/src/main/java/com/onBit/pixelDemo/ui/view/RoundRenderer.java
@@ -0,0 +1,113 @@
+package com.onBit.pixelDemo.ui.view;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+
+import com.blankj.utilcode.util.LogUtils;
+import com.github.mikephil.charting.animation.ChartAnimator;
+import com.github.mikephil.charting.buffer.BarBuffer;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.dataprovider.BarDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.model.GradientColor;
+import com.github.mikephil.charting.renderer.BarChartRenderer;
+import com.github.mikephil.charting.renderer.BarLineScatterCandleBubbleRenderer;
+import com.github.mikephil.charting.renderer.DataRenderer;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Transformer;
+import com.github.mikephil.charting.utils.Utils;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.util.List;
+
+public class RoundRenderer extends BarChartRenderer {
+    public RoundRenderer(BarDataProvider chart, ChartAnimator animator,
+                         ViewPortHandler viewPortHandler) {
+        super(chart, animator, viewPortHandler);
+        mValuePaint.setTextAlign(Paint.Align.LEFT);
+    }
+
+    private final float cornerRadius = 200f; // 圆角半径
+
+    @Override
+    protected void drawDataSet(Canvas c, IBarDataSet dataSet, int index) {
+        Transformer trans = mChart.getTransformer(dataSet.getAxisDependency());
+        final boolean drawBorder = dataSet.getBarBorderWidth() > 0.f;
+
+        float phaseX = mAnimator.getPhaseX();
+        float phaseY = mAnimator.getPhaseY();
+        // draw the bar shadow before the values
+
+        BarBuffer buffer = mBarBuffers[index];
+        buffer.setPhases(phaseX, phaseY);
+        buffer.setDataSet(index);
+        buffer.setInverted(mChart.isInverted(dataSet.getAxisDependency()));
+        buffer.setBarWidth(mChart.getBarData().getBarWidth());
+
+        buffer.feed(dataSet);
+
+        trans.pointValuesToPixel(buffer.buffer);
+
+        final boolean isSingleColor = dataSet.getColors().size() == 1;
+        if (isSingleColor) {
+            mRenderPaint.setColor(dataSet.getColor());
+        }
+        for (int j = 0; j < buffer.size(); j += 4) {
+
+            if (!mViewPortHandler.isInBoundsLeft(buffer.buffer[j + 2]))
+                continue;
+
+            if (!mViewPortHandler.isInBoundsRight(buffer.buffer[j]))
+                break;
+
+            if (!isSingleColor) {
+                // Set the color for the currently drawn value. If the index
+                // is out of bounds, reuse colors.
+                mRenderPaint.setColor(dataSet.getColor(j / 4));
+            }
+
+            if (dataSet.getGradientColor() != null) {
+                GradientColor gradientColor = dataSet.getGradientColor();
+                mRenderPaint.setShader(
+                        new LinearGradient(
+                                buffer.buffer[j],
+                                buffer.buffer[j + 3],
+                                buffer.buffer[j],
+                                buffer.buffer[j + 1],
+                                gradientColor.getStartColor(),
+                                gradientColor.getEndColor(),
+                                android.graphics.Shader.TileMode.MIRROR));
+            }
+
+            if (dataSet.getGradientColors() != null) {
+                mRenderPaint.setShader(
+                        new LinearGradient(
+                                buffer.buffer[j],
+                                buffer.buffer[j + 3],
+                                buffer.buffer[j],
+                                buffer.buffer[j + 1],
+                                dataSet.getGradientColor(j / 4).getStartColor(),
+                                dataSet.getGradientColor(j / 4).getEndColor(),
+                                android.graphics.Shader.TileMode.MIRROR));
+            }
+
+
+            c.drawRoundRect(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
+                    buffer.buffer[j + 3],cornerRadius,cornerRadius,mRenderPaint);
+
+            if (drawBorder) {
+                c.drawRect(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
+                        buffer.buffer[j + 3], mBarBorderPaint);
+            }
+        }
+
+    }
+
+}
