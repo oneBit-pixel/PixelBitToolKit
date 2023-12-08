da865db0e482d4e065e33554d48e2f22e2d67e60
zxy <9@1234>
2023-12-08 19:01:00 +0800

    1

--- app/src/main/java/com/onBit/pixelDemo/ui/activity/ChartActivity.kt
+++ app/src/main/java/com/onBit/pixelDemo/ui/activity/ChartActivity.kt
@@ -2,14 +2,15 @@ package com.onBit.pixelDemo.ui.activity
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import com.github.mikephil.charting.renderer.BarChartRenderer
 import com.github.mikephil.charting.renderer.HorizontalBarChartRenderer
 import com.onBit.PixelBitToolKit.databinding.ActivityChartBinding
 import com.onBit.lib_base.base.BaseActivity
-import com.onBit.pixelDemo.ui.dialog.MyDialog
 import com.onBit.pixelDemo.ui.view.RoundRenderer
 
 class ChartActivity : BaseActivity<ActivityChartBinding>() {
@@ -17,20 +18,19 @@ class ChartActivity : BaseActivity<ActivityChartBinding>() {
         get() = ActivityChartBinding::inflate
 
 
-    private val myDialog by lazy { MyDialog(this) }
 
     private val value by lazy {
         listOf(
             BarEntry(0f, floatArrayOf(75f, 120f)),
-            BarEntry(1f, floatArrayOf(85f,100f)),
-            BarEntry(2f, floatArrayOf(25f,100f)),
-            BarEntry(3f, floatArrayOf(65f,100f)),
-            BarEntry(4f, floatArrayOf(55f,100f)),
-            BarEntry(5f, floatArrayOf(45f,100f)),
-            BarEntry(6f, floatArrayOf(35f,100f)),
-            BarEntry(7f, floatArrayOf(45f,100f)),
-            BarEntry(8f, floatArrayOf(55f,100f)),
-            BarEntry(9f, floatArrayOf(85f,100f)),
+            BarEntry(1f, floatArrayOf(75f,130f)),
+            BarEntry(2f, floatArrayOf(75f,140f)),
+            BarEntry(3f, floatArrayOf(75f,150f)),
+            BarEntry(4f, floatArrayOf(75f,160f)),
+            BarEntry(5f, floatArrayOf(75f,170f)),
+            BarEntry(6f, floatArrayOf(75f,190f)),
+            BarEntry(7f, floatArrayOf(75f,120f)),
+            BarEntry(8f, floatArrayOf(75f,190f)),
+            BarEntry(9f, floatArrayOf(75f,300f)),
         )
     }
 
@@ -40,19 +40,41 @@ class ChartActivity : BaseActivity<ActivityChartBinding>() {
             val barDataSet = BarDataSet(value, "这是一个标题")
             barDataSet.colors = listOf(Color.TRANSPARENT,Color.BLUE)
             barDataSet.valueTextColor=Color.RED
+
             val arrayListOf = arrayListOf<IBarDataSet>(barDataSet)
             val barData = BarData(arrayListOf)
-            barData.setDrawValues(true)
-            barData.setValueTextSize(10f)
-
-            chart.setTouchEnabled(true)
+//            barData.barWidth=0.8f
+//            barData.setValueTextSize(10f)
             chart.data = barData
             chart.renderer=RoundRenderer(chart,chart.animator,chart.viewPortHandler)
-            chart.setDrawValueAboveBar(true)
 
-            chart.setOnClickListener {
-                myDialog.show()
-            }
+//            chart.setTouchEnabled(true)
+//            chart.setDrawValueAboveBar(true)
+//            chart.isDragXEnabled=true
+//            chart.xAxis.apply {
+//                position=XAxis.XAxisPosition.BOTTOM
+//                setDrawGridLines(false)
+//                setDrawLabels(false)
+//                axisMinimum=-5f
+//                axisMaximum=chart.data.entryCount+5f
+//            }
+//            chart.setVisibleXRangeMaximum(7f)
+//            chart.moveViewToX(2f)
+//            chart.axisRight.apply {
+//                isEnabled=false
+//                setDrawLabels(false)
+//            }
+//            chart.axisLeft.apply {
+//                isEnabled=true
+//                setDrawAxisLine(false)
+//                setDrawGridLines(true)
+//                enableGridDashedLine(20f,10f,0f)
+//                axisMaximum=400f
+//                axisMinimum=35f
+//                setLabelCount(6,true)
+//            }
+//            chart.invalidate()
+
         }
 
     }
--- app/src/main/java/com/onBit/pixelDemo/ui/view/RoundRenderer.java
+++ app/src/main/java/com/onBit/pixelDemo/ui/view/RoundRenderer.java
@@ -1,31 +1,23 @@
 package com.onBit.pixelDemo.ui.view;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.graphics.drawable.Drawable;
 
-import com.blankj.utilcode.util.LogUtils;
 import com.github.mikephil.charting.animation.ChartAnimator;
 import com.github.mikephil.charting.buffer.BarBuffer;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.dataprovider.BarDataProvider;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.model.GradientColor;
 import com.github.mikephil.charting.renderer.BarChartRenderer;
-import com.github.mikephil.charting.renderer.BarLineScatterCandleBubbleRenderer;
-import com.github.mikephil.charting.renderer.DataRenderer;
-import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Fill;
 import com.github.mikephil.charting.utils.Transformer;
 import com.github.mikephil.charting.utils.Utils;
 import com.github.mikephil.charting.utils.ViewPortHandler;
-
-import java.util.List;
 
 public class RoundRenderer extends BarChartRenderer {
     public RoundRenderer(BarDataProvider chart, ChartAnimator animator,
@@ -35,79 +27,114 @@ public class RoundRenderer extends BarChartRenderer {
     }
 
     private final float cornerRadius = 200f; // 圆角半径
-
-    @Override
-    protected void drawDataSet(Canvas c, IBarDataSet dataSet, int index) {
-        Transformer trans = mChart.getTransformer(dataSet.getAxisDependency());
-        final boolean drawBorder = dataSet.getBarBorderWidth() > 0.f;
-
-        float phaseX = mAnimator.getPhaseX();
-        float phaseY = mAnimator.getPhaseY();
-        // draw the bar shadow before the values
-
-        BarBuffer buffer = mBarBuffers[index];
-        buffer.setPhases(phaseX, phaseY);
-        buffer.setDataSet(index);
-        buffer.setInverted(mChart.isInverted(dataSet.getAxisDependency()));
-        buffer.setBarWidth(mChart.getBarData().getBarWidth());
-
-        buffer.feed(dataSet);
-
-        trans.pointValuesToPixel(buffer.buffer);
-
-        final boolean isSingleColor = dataSet.getColors().size() == 1;
-        if (isSingleColor) {
-            mRenderPaint.setColor(dataSet.getColor());
-        }
-        for (int j = 0; j < buffer.size(); j += 4) {
-
-            if (!mViewPortHandler.isInBoundsLeft(buffer.buffer[j + 2]))
-                continue;
-
-            if (!mViewPortHandler.isInBoundsRight(buffer.buffer[j]))
-                break;
-
-            if (!isSingleColor) {
-                // Set the color for the currently drawn value. If the index
-                // is out of bounds, reuse colors.
-                mRenderPaint.setColor(dataSet.getColor(j / 4));
-            }
-
-            if (dataSet.getGradientColor() != null) {
-                GradientColor gradientColor = dataSet.getGradientColor();
-                mRenderPaint.setShader(
-                        new LinearGradient(
-                                buffer.buffer[j],
-                                buffer.buffer[j + 3],
-                                buffer.buffer[j],
-                                buffer.buffer[j + 1],
-                                gradientColor.getStartColor(),
-                                gradientColor.getEndColor(),
-                                android.graphics.Shader.TileMode.MIRROR));
-            }
-
-            if (dataSet.getGradientColors() != null) {
-                mRenderPaint.setShader(
-                        new LinearGradient(
-                                buffer.buffer[j],
-                                buffer.buffer[j + 3],
-                                buffer.buffer[j],
-                                buffer.buffer[j + 1],
-                                dataSet.getGradientColor(j / 4).getStartColor(),
-                                dataSet.getGradientColor(j / 4).getEndColor(),
-                                android.graphics.Shader.TileMode.MIRROR));
-            }
+    private RectF mBarShadowRectBuffer = new RectF();
 
 
-            c.drawRoundRect(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
-                    buffer.buffer[j + 3],cornerRadius,cornerRadius,mRenderPaint);
 
-            if (drawBorder) {
-                c.drawRect(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
-                        buffer.buffer[j + 3], mBarBorderPaint);
-            }
-        }
+    //    @Override
+//    protected void drawDataSet(Canvas c, IBarDataSet dataSet, int index) {
+//
+//        Transformer trans = mChart.getTransformer(dataSet.getAxisDependency());
+//
+//        mBarBorderPaint.setColor(dataSet.getBarBorderColor());
+//        mBarBorderPaint.setStrokeWidth(Utils.convertDpToPixel(dataSet.getBarBorderWidth()));
+//
+//        final boolean drawBorder = dataSet.getBarBorderWidth() > 0.f;
+//
+//        float phaseX = mAnimator.getPhaseX();
+//        float phaseY = mAnimator.getPhaseY();
+//
+//        // draw the bar shadow before the values
+//        if (mChart.isDrawBarShadowEnabled()) {
+//            mShadowPaint.setColor(dataSet.getBarShadowColor());
+//
+//            BarData barData = mChart.getBarData();
+//
+//            final float barWidth = barData.getBarWidth();
+//            final float barWidthHalf = barWidth / 2.0f;
+//            float x;
+//
+//            for (int i = 0, count = Math.min((int)(Math.ceil((float)(dataSet.getEntryCount()) * phaseX)), dataSet.getEntryCount());
+//                 i < count;
+//                 i++) {
+//
+//                BarEntry e = dataSet.getEntryForIndex(i);
+//
+//                x = e.getX();
+//
+//                mBarShadowRectBuffer.left = x - barWidthHalf;
+//                mBarShadowRectBuffer.right = x + barWidthHalf;
+//
+//                trans.rectValueToPixel(mBarShadowRectBuffer);
+//
+//                if (!mViewPortHandler.isInBoundsLeft(mBarShadowRectBuffer.right))
+//                    continue;
+//
+//                if (!mViewPortHandler.isInBoundsRight(mBarShadowRectBuffer.left))
+//                    break;
+//
+//                mBarShadowRectBuffer.top = mViewPortHandler.contentTop();
+//                mBarShadowRectBuffer.bottom = mViewPortHandler.contentBottom();
+//
+//                c.drawRect(mBarShadowRectBuffer, mShadowPaint);
+//            }
+//        }
+//
+//        // initialize the buffer
+//        BarBuffer buffer = mBarBuffers[index];
+//        buffer.setPhases(phaseX, phaseY);
+//        buffer.setDataSet(index);
+//        buffer.setInverted(mChart.isInverted(dataSet.getAxisDependency()));
+//        buffer.setBarWidth(mChart.getBarData().getBarWidth());
+//
+//        buffer.feed(dataSet);
+//
+//        trans.pointValuesToPixel(buffer.buffer);
+//
+//        final boolean isCustomFill = dataSet.getFills() != null && !dataSet.getFills().isEmpty();
+//        final boolean isSingleColor = dataSet.getColors().size() == 1;
+//        final boolean isInverted = mChart.isInverted(dataSet.getAxisDependency());
+//
+//        if (isSingleColor) {
+//            mRenderPaint.setColor(dataSet.getColor());
+//        }
+//
+//        for (int j = 0, pos = 0; j < buffer.size(); j += 4, pos++) {
+//
+//            if (!mViewPortHandler.isInBoundsLeft(buffer.buffer[j + 2]))
+//                continue;
+//
+//            if (!mViewPortHandler.isInBoundsRight(buffer.buffer[j]))
+//                break;
+//
+//            if (!isSingleColor) {
+//                // Set the color for the currently drawn value. If the index
+//                // is out of bounds, reuse colors.
+//                mRenderPaint.setColor(dataSet.getColor(pos));
+//            }
+//
+//            if (isCustomFill) {
+//                dataSet.getFill(pos)
+//                        .fillRect(
+//                                c, mRenderPaint,
+//                                buffer.buffer[j],
+//                                buffer.buffer[j + 1],
+//                                buffer.buffer[j + 2],
+//                                buffer.buffer[j + 3],
+//                                isInverted ? Fill.Direction.DOWN : Fill.Direction.UP);
+//            }
+//            else {
+//                c.drawRect(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
+//                        buffer.buffer[j + 3], mRenderPaint);
+//            }
+//
+//            if (drawBorder) {
+//                c.drawRoundRect(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
+//                        buffer.buffer[j + 3],cornerRadius,cornerRadius, mBarBorderPaint);
+//            }
+//        }
+//
+//    }
 
-    }
 
 }
